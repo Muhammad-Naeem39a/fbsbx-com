04c4f66665754d0fa1caafaf0bd002489aebae76
na_su <muhmmadnaeem39a@gmail.com>
2022-08-25 12:20:37 +0500

    Finalizing the app

--- src/components/body.js
+++ src/components/body.js
@@ -10,22 +10,22 @@ function Body() {
         <div className=''> 
             <div className='w-full flex relative overflow-hidden cursor-pointer md:pb-10'>
                 <img src={sidedots} className=' content-end h-24 mt-2 mr-16 md:w-24' />
-                <div class="absolute md:top-1/2 xs:top-10 px-6 py-4 -mt-12 z-30">
-                    <h1 class="absolute mb-3 text-xl ml-4 font-semibold md:-top-4 left-1 tracking-tight text-black">MALO Reinigung GmbH</h1>
-                    <p class="leading-normal content-start w-10/12 text-black -ml-2">Lorem ipsum dolor, sit amet cons ectetur adipis icing elit. Praesen tium</p>
+                <div className="absolute md:top-1/2 xs:top-10 px-6 py-4 -mt-12 z-30">
+                    <h1 className="absolute mb-3 text-xl ml-4 font-semibold md:-top-4 left-1 tracking-tight text-black">MALO Reinigung GmbH</h1>
+                    <p className="leading-normal content-start w-10/12 text-black -ml-2">Lorem ipsum dolor, sit amet cons ectetur adipis icing elit. Praesen tium</p>
                 </div>
                 <img src={shoppingmall} className="flex md:h-64 mt-2 md:w-72 opacity-90" />
             </div>
             <div className='w-full flex md:pb-10'>
                 <img src={mop} className="flex h-64 mt-2 w-72 opacity-50" />
-                <div class="absolute md:top-1/2 xs:top-10 right-2 px-6 py-4 -mt-12 z-70">
-                    <h1 class="mb-3 text-xl ml-4 font-semibold tracking-tight w-52 text-blue-400">ERLEBEN SIE</h1>
-                    <h1 class="mb-3 text-xl ml-4 font-semibold tracking-tight w-52 text-blue-900">FRISCHE UND</h1>
-                    <h1 class="mb-3 text-xl ml-4 font-semibold tracking-tight w-52 text-blue-400">REINHEIT</h1>
+                <div className="absolute md:top-1/2 xs:top-10 right-2 px-6 py-4 -mt-12 z-70">
+                    <h1 className="mb-3 text-xl ml-4 font-semibold tracking-tight w-52 text-blue-400">ERLEBEN SIE</h1>
+                    <h1 className="mb-3 text-xl ml-4 font-semibold tracking-tight w-52 text-blue-900">FRISCHE UND</h1>
+                    <h1 className="mb-3 text-xl ml-4 font-semibold tracking-tight w-52 text-blue-400">REINHEIT</h1>
                 </div>
             </div>
             <div className='w-full flex justify-end content-center absolute top-64 -right-10'>
-            <img src={drop} className="-z-20 flex mt-2 h-1/12 w-4/6 float-right" />
+            <img src={drop} className="-z-20 flex mt-8 mr-8 h-4/12 w-3/6" />
             </div>
         </div>
     );
